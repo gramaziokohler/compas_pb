adf6ddf96b4dbb15591ee1e22c2e6da55f00e22c
WeiTing1991 <>
2025-04-06 21:45:31 +0200

    add protobuffer class inherit from compas data

--- src/compas_pb/data/data.py
+++ src/compas_pb/data/data.py
@@ -1,12 +1,13 @@
 from compas.data import Data
-from compas.geometry import Point, Vector
+from compas.geometry import Frame, Line, Point
 
-from compas_pb.data import point_pb2 as PointData
-from compas_pb.data import vector_pb2 as VectorData
-# from compas_pb.data import line_pb2 as LineData
-# from compas_pb.data import frame_pb2 as FrameData
-# from compas_pb.data import element_pb2 as ElementData
-from compas_pb.data import message_pb2 as AnyData
+# from compas_model.elements import Element
+from compas_pb.data.proto import element_pb2 as ElementData
+from compas_pb.data.proto import frame_pb2 as FrameData
+from compas_pb.data.proto import line_pb2 as LineData
+from compas_pb.data.proto import message_pb2 as AnyData
+from compas_pb.data.proto import point_pb2 as PointData
+from compas_pb.data.proto import vector_pb2 as VectorData
 
 
 class ProtoBufferData(Data):
@@ -24,11 +25,11 @@ class ProtoBufferData(Data):
     # ==========================================================================
 
     @property
-    def data_offset(self):
+    def proto_data(self):
         return self._proto_data
 
-    @data_offset.setter
-    def data_offset(self, data):
+    @proto_data.setter
+    def proto_data(self, data):
         self._proto_data = data
 
     @property
@@ -39,28 +40,61 @@ class ProtoBufferData(Data):
     def type_enum(self, data):
         self._type_enum = data
 
-    def get_proto_fields(self):
-        if self._proto_data is None:
-            raise ValueError("Proto data not set")
-        return self._proto_data.DESCRIPTOR.fields_by_name.keys()
+    @property
+    def __data__(self) -> dict:
+        """Get the data as dictionary representation."""
+        from google.protobuf.json_format import MessageToDict
+
+        proto_dict = None
+        if self._proto_data is not None:
+            proto_dict = MessageToDict(self._proto_data)
+
+        return {
+            "proto_data": proto_dict,
+            "type_enum": self._type_enum,
+        }
 
     # ==========================================================================
     # Methods
     # ==========================================================================
 
-    def _to_pb(self, obj):
+    def to_pb(self, obj):
         raise NotImplementedError("Subclasses implement this method")
 
-class ProtoBufferPoint(ProtoBufferData):
-    """
+    @staticmethod
+    def from_pb(proto_data):
+        raise NotImplementedError("Subclasses implement this method")
 
-    """
+    def get_proto_fields(self):
+        if self._proto_data is None:
+            raise ValueError("Proto data not set")
+        return self._proto_data.DESCRIPTOR.fields_by_name.keys()
+
+    def get_dtype(self, obj=None):
+        """Get the data type of the object."""
+        if self._proto_data is None:
+            raise ValueError("Proto data not set")
+        return AnyData.DataType.Name(self._type_enum).lower()
+
+
+class ProtoBufferPoint(ProtoBufferData):
+    """ """
+
     def __init__(self, name=None):
         super(ProtoBufferPoint, self).__init__(name=name)
         self._proto_data = PointData.PointData()
         self._type_enum = AnyData.DataType.POINT
 
-    def _to_pb(self, obj):
+        # if obj is not None:
+        #     proto_data, type_enum = self._to_pb(obj)
+        #     self.proto_data = proto_data
+        #     self.type_enum = type_enum
+
+        # self.proto_data = self._proto_data
+        # self.type_enum = self._type_enum
+
+    def to_pb(self, obj):
+        """Convert a Point object to a protobuf message."""
 
         self._proto_data.guid = str(obj.guid)
         self._proto_data.name = obj.name
@@ -70,17 +104,69 @@ class ProtoBufferPoint(ProtoBufferData):
 
         return self._proto_data, self._type_enum
 
-class ProtoBufferVector(ProtoBufferData):
-    """
+    @staticmethod
+    def from_pb(proto_data):
+        """Convert a protobuf message to a Point object."""
+        # field_name = AnyData.DataType.Name(proto_data.type).lower()
+        # from google.protobuf.json_format import MessageToDict
+        point_data = None
+        if hasattr(proto_data, "point"):
+            point_data = Point(
+                proto_data.point.x,
+                proto_data.point.y,
+                proto_data.point.z,
+            )
+        else:
+            point_data = Point(
+                proto_data.x,
+                proto_data.y,
+                proto_data.z,
+            )
+        return point_data
 
-    """
-    def __init__(self, name=None):
+
+class ProtoBufferLine(ProtoBufferData):
+    """ """
+
+    def __init__(self, obj=None, name=None):
+        super(ProtoBufferLine, self).__init__(name=name)
+        self._proto_data = LineData.LineData()
+        self._type_enum = AnyData.DataType.LINE
+
+    def to_pb(self, obj):
+        self._proto_data.guid = str(obj.guid)
+        self._proto_data.name = obj.name
+        start, _ = ProtoBufferPoint().to_pb(obj.start)
+        end, _ = ProtoBufferPoint().to_pb(obj.end)
+
+        self._proto_data.start.CopyFrom(start)
+        self._proto_data.end.CopyFrom(end)
+
+        return self._proto_data, self._type_enum
+
+    @staticmethod
+    def from_pb(proto_data):
+        """Convert a protobuf message to a Line object."""
+        start = ProtoBufferPoint.from_pb(proto_data.line.start)
+        end = ProtoBufferPoint.from_pb(proto_data.line.end)
+        line_data = Line(start, end)
+        return line_data
+
+
+class ProtoBufferVector(ProtoBufferData):
+    """ """
+
+    def __init__(self, obj=None, name=None):
         super(ProtoBufferVector, self).__init__(name=name)
         self._proto_data = VectorData.VectorData()
         self._type_enum = AnyData.DataType.VECTOR
 
-    def _to_pb(self, obj):
+        if obj is not None:
+            proto_data, type_enum = self.to_pb(obj)
+            self.proto_data = proto_data
+            self.type_enum = type_enum
 
+    def to_pb(self, obj):
         self._proto_data.guid = str(obj.guid)
         self._proto_data.name = obj.name
         self._proto_data.x = obj.x
@@ -89,66 +175,133 @@ class ProtoBufferVector(ProtoBufferData):
 
         return self._proto_data, self._type_enum
 
-
-# def line_to_pb(self, obj):
-#     """Convert a Line object to a protobuf message."""
-#
-#     if not isinstance(obj, Line):
-#         raise TypeError("Expected a Line object")
-#
-#     line_data = LineData.LineData()
-#     start, _ = self.point_to_pb(obj.start)
-#     end, _ = self.point_to_pb(obj.end)
-#
-#     line_data.guid = str(obj.guid)
-#     line_data.name = obj.name
-#     line_data.start.CopyFrom(start)
-#     line_data.end.CopyFrom(end)
-#
-#     return line_data, AnyData.DataType.LINE
-
-
-# def frame_to_pb(self, obj):
-#     if not isinstance(obj, Frame):
-#         raise TypeError("Expected a Frame object")
-#
-#     frame_data = FrameData.FrameData()
-#     point, _ = self.point_to_pb(obj.point)
-#     xaxis, _ = self.vector_to_pb(obj.xaxis)
-#     yaxis, _ = self.vector_to_pb(obj.yaxis)
-#
-#     frame_data.guid = str(obj.guid)
-#     frame_data.name = obj.name
-#     frame_data.point.CopyFrom(point)
-#     frame_data.xaxis.CopyFrom(xaxis)
-#     frame_data.yaxis.CopyFrom(yaxis)
-#
-#     return frame_data, AnyData.DataType.FRAME
+    @staticmethod
+    def from_pb(proto_data):
+        """Convert a protobuf message to a Vector object."""
+        # field_name = AnyData.DataType.Name(proto_data.type).lower()
+        # from google.protobuf.json_format import MessageToDict
+        vector_data = None
+        if hasattr(proto_data, "vector"):
+            vector_data = Point(
+                proto_data.vector.x,
+                proto_data.vector.y,
+                proto_data.vector.z,
+            )
+        else:
+            vector_data = Point(
+                proto_data.x,
+                proto_data.y,
+                proto_data.z,
+            )
+        return vector_data
 
 
-# def element_to_pb(self, obj):
-#     element_data = ElementData.ElementData()
-#     frame, _ = self.frame_to_pb(obj.frame)
-#
-#     if not isinstance(obj, Element):
-#         raise TypeError("Expected a Element object")
-#
-#     element_data.guid = str(obj.guid)
-#     element_data.name = obj.name
-#     element_data.frame.CopyFrom(frame)
-#     element_data.xsize = obj.xsize
-#     element_data.ysize = obj.ysize
-#     element_data.zsize = obj.zsize
-#
-#     return element_data, AnyData.DataType.ELEMENT
+class ProtoBufferFrame(ProtoBufferData):
+    """ """
+
+    def __init__(self, obj=None, name=None):
+        super(ProtoBufferFrame, self).__init__(name=name)
+        self._proto_data = FrameData.FrameData()
+        self._type_enum = AnyData.DataType.FRAME
+
+        if obj is not None:
+            proto_data, type_enum = self.to_pb(obj)
+            self.proto_data = proto_data
+            self.type_enum = type_enum
+
+    def to_pb(self, obj):
+        """Convert a Frame object to a protobuf message."""
+        self._proto_data.guid = str(obj.guid)
+        self._proto_data.name = obj.name
+        point, _ = ProtoBufferPoint().to_pb(obj.point)
+        xaxis, _ = ProtoBufferVector().to_pb(obj.xaxis)
+        yaxis, _ = ProtoBufferVector().to_pb(obj.yaxis)
+
+        self._proto_data.point.CopyFrom(point)
+        self._proto_data.xaxis.CopyFrom(xaxis)
+        self._proto_data.yaxis.CopyFrom(yaxis)
+
+        return self._proto_data, self._type_enum
+
+    @staticmethod
+    def from_pb(proto_data):
+        """Convert a protobuf message to a Vector object."""
+        point = ProtoBufferPoint.from_pb(proto_data.frame.point)
+        xaxis = ProtoBufferVector.from_pb(proto_data.frame.xaxis)
+        yaxis = ProtoBufferVector.from_pb(proto_data.frame.yaxis)
+        frame = Frame(
+            point=point,
+            xaxis=xaxis,
+            yaxis=yaxis,
+        )
+        return frame
+
+
+class ProtoBufferElement(ProtoBufferData):
+    """ """
+
+    def __init__(self, obj=None, name=None):
+        super(ProtoBufferElement, self).__init__(name=name)
+        self._proto_data = ElementData.ElementData()
+        self._type_enum = AnyData.DataType.ELEMENT
+
+        if obj is not None:
+            proto_data, type_enum = self.to_pb(obj)
+            self.proto_data = proto_data
+            self.type_enum = type_enum
+
+    def to_pb(self, obj):
+        """Convert a Element object to a protobuf message."""
+        self._proto_data.guid = str(obj.guid)
+        self._proto_data.name = obj.name
+        frame, _ = ProtoBufferFrame().to_pb(obj.frame)
+
+        self._proto_data.frame.CopyFrom(frame)
+        self._proto_data.xsize = obj.xsize
+        self._proto_data.ysize = obj.ysize
+        self._proto_data.zsize = obj.zsize
+
+        return self._proto_data, self._type_enum
+
+    # TODO:
+    # @staticmethod
+    # def from_pb(proto_data):
+    #     """Convert a protobuf message to a Vector object."""
+    #     element = Element()
+    #     return frame
+
+
+class ProtoBufferAny(ProtoBufferData):
+    def __init__(self, name=None):
+        super(ProtoBufferAny, self).__init__(name=name)
+        self._proto_data = AnyData.AnyData()
+        self._type_enum = AnyData.DataType.UNKNOWN
+
+
+class ProtoBufferList(ProtoBufferData):
+    """ """
+
+    def __init__(self, obj=None, name=None):
+        super(ProtoBufferList, self).__init__(name=name)
+        self._proto_data = AnyData.ListData()
+        self._type_enum = AnyData.DataType.LIST
+
+
+class ProtoBufferDict(ProtoBufferData):
+    """ """
+
+    def __init__(self, obj=None, name=None):
+        super(ProtoBufferDict, self).__init__(name=name)
+        self._proto_data = AnyData.DictData()
+        self._type_enum = AnyData.DataType.DICT
 
 
 if __name__ == "__main__":
-
     point = Point(1, 2, 3)
-    point_data = ProtoBufferData().point_to_pb(point)
-    print(point_data)
 
-    vector = Vector(1, 2, 3)
-    vector_data = ProtoBufferData().vector_to_pb(vector)
-    print(vector_data)
+    point_data = ProtoBufferPoint()
+    point_data.to_pb(point)
+    print(point_data.proto_data)
+
+    # line = Line(Point(1, 2, 3), Point(4, 5, 6))
+    # line_data = ProtoBufferLine(line)
