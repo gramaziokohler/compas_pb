c896e3aa973a88cdada6082f09f6199d4fc996cb
Chen Kasirer <ckasirer@ethz.ch>
2025-04-29 18:21:51 +0200

    csharp example

--- cSharp/CompasProtobuffer/Program.cs
+++ cSharp/CompasProtobuffer/Program.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
 using CompasPb.Data;
-using CompasPb.Data.Processor;
-using Google.Protobuf;
 
 
 public class Program
@@ -20,69 +13,47 @@ public class Program
         }
         else
         {
-            filePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "examples", "temp", "nested_data.bin");
-            filePath = Path.GetFullPath(filePath);
+            filePath = @"C:\Users\ckasirer\repos\compas_pb\examples\temp\data_dict.bin";
         }
+
+        AnyData data = CompasPbApi.pbLoad(filePath);
+        //AnyData? messageData = pbLoadBytes(filePath);
+        //AnyData? messageData = pbLoadJSON(filePath);
+
+        // var point = TryGetValue<Point>(data);
+        var listOfPoints = TryGetValue<List<Point>>(data);
+        // var dictOfStringsToPoints = TryGetValue<Dict<string, Point>>(data);
+        // var listOfWhatever = TryGetValue<List<AnyData>>(data); // we don't deal with deeper nesting levels, sue us.
+
+        foreach(var point in listOfPoints){
+            Console.WriteLine($"Item: {point.X: F2}, {point.Y:F2}, {point.Z:F2}");
+        }
+    }
+
+}
+
+class CompasPbApi
+{
+    public T TryGetValue<T>(AnyData obj)
+    {
+
+        throw new InvalidCastException($"Cannot cast {obj.GetType()} to {typeof(T)}");
+    }
+    // this is part of the COMPAS Protobuf csharp library
+    public static AnyData pbLoad(string filePath){
         Console.WriteLine($"Processing file: {filePath}");
         if (!File.Exists(filePath))
         {
             Console.WriteLine($"Error: File does not exist at path: {filePath}");
-            return;
+            // raise exception!
         }
 
+        // Abstract away file reading
         byte[] data = File.ReadAllBytes(filePath);
-        DataDeserializer dataDeserializer = new DataDeserializer(data);
-        dynamic deserializedData = dataDeserializer.DeserializeMessage();
+        var message = MessageData.Parser.ParseFrom(data);
 
-        Console.WriteLine($"Data Type: {deserializedData.GetType()}");
-        // Console.WriteLine("\nPress any key to exit...");
-        // Console.ReadKey();
+        // abstract away MessageData.Data
+        return message.Data;  // this can come from us
     }
-}
 
-// public class ProtoBufProcessor
-// {
-//     public void ProcessProtoMessage(string filePath)
-//     {
-//         try
-//         {
-//             byte[] data = File.ReadAllBytes(filePath);
-//             var message = CompasPb.Data.MessageData.Parser.ParseFrom(data);
-//
-//             Console.WriteLine($"Message Type: {message.GetType()}\n");
-//             Console.WriteLine($"Message: {message}\n");
-//
-//             // convert to JSON using Google's built-in formatter
-//             string jsonString = Google.Protobuf.JsonFormatter.Default.Format(message);
-//             Console.WriteLine($"JSON representation:\n{jsonString}\n");
-//
-//             var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true };
-//             // Deserialize JSON to a dictionary
-//             var dictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonString, options);
-//             if (dictionary == null)
-//             {
-//                 Console.WriteLine("Failed to deserialize JSON to dictionary.");
-//                 return;
-//             }
-//             Console.WriteLine("Full message as dictionary:");
-//             Console.WriteLine(dictionary.GetType());
-//             foreach (var entry in dictionary)
-//             {
-//                 Console.WriteLine($"{entry.Key}: {entry.Value}");
-//             }
-//             // print nested data
-//             if (dictionary.TryGetValue("data", out object dataObj))
-//             {
-//                 var dataJson = JsonSerializer.Serialize(dataObj, options);
-//                 Console.WriteLine($"\nNested Data Type: {dataJson.GetType()}");
-//                 Console.WriteLine($"Nested Data: {dataJson}");
-//             }
-//         }
-//         catch (Exception ex)
-//         {
-//             Console.WriteLine($"Error processing file: {ex.Message}");
-//             Console.WriteLine($"Stack trace: {ex.StackTrace}");
-//         }
-//     }
-//
-// }
+}
