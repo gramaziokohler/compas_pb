c680e0a7c7d5753cd16d43efa4ec25c41a3466b9
WeitingChen <weitingchen0502@gmail.com>
2025-05-12 16:30:34 +0200

    test with google protobuf any type

--- examples/example.py
+++ examples/example.py
@@ -10,6 +10,10 @@ from compas_pb.data.data_handling import pb_load
 from compas_pb.data.data_handling import pb_load_bts
 from compas_pb.data.data_handling import pb_load_json
 
+from google.protobuf.any_pb2 import Any
+from google.protobuf.struct_pb2 import Struct
+from compas_pb.data.proto import message_pb2 as AnyData
+
 
 def main():
     # Example nested data structure
@@ -22,37 +26,51 @@ def main():
         # "polyline": Polyline([[0, 0, 0], [1, 0, 0], [2, 0, 0], [3, 0, 0]])
         # "element": element,
     }
-
     nested_data_diff_types = {
         "point": Point(1.0, 2.0, 3.0),
-        "polyline": Polyline([[0, 0, 0], [1, 0, 0], [2, 0, 0], [3, 0, 0]]),
-        "list": ["hey", [0.0, 0.5, 1.5], True, 5, 10]
+        # "polyline": Polyline([[0, 0, 0], [1, 0, 0], [2, 0, 0], [3, 0, 0]]),
+        # "list": ["hey", [0.0, 0.5, 1.5], True, 5, 10]
     }
 
-    FILEPATH = Path(__file__).parent / "temp" / "nested_data.bin"
+    point = Point(1.0, 2.0, 3.0)
+    struct = Struct()
+    struct.update(point.__jsondump__())
+    any_data = AnyData.AnyMessageData()
+    any_data.data.Pack(struct)
+    print(any_data)
+
+
+    uppacke_struct = Struct()
+    any_data.data.Unpack(uppacke_struct)
+    print(dict(uppacke_struct))
+
+    # data = AnyData.AnyMessageData().data.Pack(point)
+    # print(f"data_type: {type(data)}")
+
+    # FILEPATH = Path(__file__).parent / "temp" / "nested_data.bin"
 
     # LOAD THE DATA FROM THE FILE
-    pb_dump(nested_data_diff_types, filepath=FILEPATH.as_posix())
+    # pb_dump(nested_data_diff_types, filepath=FILEPATH.as_posix())
 
-    # DESERIALIZE THE DATA
-    loaded_data = pb_load(filepath=FILEPATH.as_posix())
-    print(f"pb_load type: {type(loaded_data)}")
-    print(f"loaded_data_from_bin: {loaded_data}\n")
+    # # DESERIALIZE THE DATA
+    # loaded_data = pb_load(filepath=FILEPATH.as_posix())
+    # print(f"pb_load type: {type(loaded_data)}")
+    # print(f"loaded_data_from_bin: {loaded_data}\n")
 
-    # TO JSONSTRING AND FROM JSONSTRING
-    data_json_string = pb_dump_json(nested_data_diff_types)
-    print(f"data_json_string type: {type(data_json_string)}")
-    # print(f"data_json_string: {data_json_string}\n")
+    # # TO JSONSTRING AND FROM JSONSTRING
+    # data_json_string = pb_dump_json(nested_data_diff_types)
+    # print(f"data_json_string type: {type(data_json_string)}")
+    # # print(f"data_json_string: {data_json_string}\n")
 
-    loaded_data_json = pb_load_json(data_json_string)
-    print(f"loaded_data_from_json type: {type(loaded_data_json)}")
-    # print(f"loaded_data_from_json: {loaded_data_json}")
+    # loaded_data_json = pb_load_json(data_json_string)
+    # print(f"loaded_data_from_json type: {type(loaded_data_json)}")
+    # # print(f"loaded_data_from_json: {loaded_data_json}")
 
-    # TO BTS AND FROM BTS
-    data_bts = pb_dump_bts(nested_data_diff_types)
-    print(f"data_bts type: {type(data_bts)}")
-    loaded_data_bts = pb_load_bts(data_bts)
-    print(f"loaded_data_from_bts type: {type(loaded_data_bts)}")
+    # # TO BTS AND FROM BTS
+    # data_bts = pb_dump_bts(nested_data_diff_types)
+    # print(f"data_bts type: {type(data_bts)}")
+    # loaded_data_bts = pb_load_bts(data_bts)
+    # print(f"loaded_data_from_bts type: {type(loaded_data_bts)}")
 
 
 if __name__ == "__main__":
