33c3c179469ca732367e6d4a0982b118c3c69e2b
Chen Kasirer <ckasirer@ethz.ch>
2025-07-16 11:55:11 +0200

    removed root imported, not needed

--- src/compas_pb/__init__.py
+++ src/compas_pb/__init__.py
@@ -2,9 +2,6 @@ from __future__ import print_function
 
 import os
 
-# this is imported for side effects, to register the protobuf types
-import compas_pb.data.conversions  # type: ignore # noqa: F401
-
 
 __author__ = ["Chen Kasirer"]
 __copyright__ = "Gramazio Kohler Research"
