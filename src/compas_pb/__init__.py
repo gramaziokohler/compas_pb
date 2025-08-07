58e4a842126fad636bf567d0a972c96f54bb5545
Chen Kasirer <ckasirer@ethz.ch>
2025-08-07 11:58:59 +0200

    Bump version to 0.1.1

--- src/compas_pb/__init__.py
+++ src/compas_pb/__init__.py
@@ -17,7 +17,7 @@ __author__ = ["Wei-Ting Chen", "Chen Kasirer"]
 __copyright__ = "Gramazio Kohler Research"
 __license__ = "MIT License"
 __email__ = "kasirer@arch.ethz.ch"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 HERE = os.path.dirname(__file__)
