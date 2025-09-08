f136ef728b1330aa6c26d7ea01aa7817e747859e
Chen Kasirer <ckasirer@ethz.ch>
2025-09-08 15:28:09 +0200

    moved IDL folder into the library so that it can be installed with it

--- src/compas_pb/__init__.py
+++ src/compas_pb/__init__.py
@@ -2,9 +2,6 @@ from __future__ import print_function
 
 import os
 
-# please don't remove me! I trigger the registration of all the core serializers
-import compas_pb.conversions  # noqa: F401  #
-
 from .api import pb_load
 from .api import pb_dump
 from .api import pb_dump_bts
@@ -26,7 +23,7 @@ HOME = os.path.abspath(os.path.join(HERE, "../../"))
 DATA = os.path.abspath(os.path.join(HOME, "data"))
 DOCS = os.path.abspath(os.path.join(HOME, "docs"))
 TEMP = os.path.abspath(os.path.join(HOME, "temp"))
-IDL = os.path.abspath(os.path.join(HOME, "IDL"))
+IDL = os.path.abspath(os.path.join(HERE, "protobuf_defs"))
 
 __all__ = [
     "HOME",
